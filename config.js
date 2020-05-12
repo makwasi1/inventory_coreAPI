85296cc7e691347a57de28be4ee5f0a31464702e
Patricia <pmubiru2020@gmail.com>
2020-05-12 13:44:52 +0300

    database successfully connected

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
 //connecting to mongodb database
 //import the mongoose module
 var mongoose = require('mongoose');
+require("dotenv").config();
+
 
 //set up default mongoose connection
-var mongodb = 'mongodb://127.0.0.1/my_database';
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+var mongodb = process.env.URL;
+mongoose.connect(mongodb, {useNewUrlParser: true,useUnifiedTopology:true});
 
 //get default connection
 var db = mongoose.connection;
 
 //Bind connection to error event(to get notification of connection errors)
-db.on('error',console.error.bind(console,'MongoDB connection error:'));
+db.once("open",()=>{
+    console.log('Connection successful')
+})
+
+db.on("error",()=>{
+    console.log('Connection failed')
+})
+
+module.exports = db;
